a3018fa6cf6662ee90e4d6e9dd7d413a500976cb
olga <olga-alekhina@rambler.ru>
2024-11-11 01:43:13 +0300

    fix url for get contest method

--- step/contests/urls.py
+++ step/contests/urls.py
@@ -4,6 +4,6 @@ from .views import ArchiveView, ContestDetailsView, ContestsView
 
 urlpatterns = [
     path('api/step/contests/archieve/', ArchiveView.as_view(), name='archive'),
-    path('api/contests/<uuid:contest_id>/', ContestDetailsView.as_view(), name='contest_details'),
+    path('api/step/contests/<uuid:contest_id>/', ContestDetailsView.as_view(), name='contest_details'),
     path('api/contests/<uuid:process_id>/', ContestsView.as_view(), name='contests'),
 ]
